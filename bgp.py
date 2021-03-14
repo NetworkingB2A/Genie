46c9c528e6aaefdcfb108fb4c6628ec420bc9d02
Adam Angell <adam@networkingb2a.com>
2021-03-13 20:54:24 -0600

    cleaning up config

---
+++ bgp.py
@@ -0,0 +1,23 @@
+#!./venv/bin/python3 
+from genie.testbed import load
+from pprint import pprint
+from nested_lookup import nested_lookup
+import csv
+
+tb = load('./testbed/testbed.yml')
+outputDict= {}
+
+for router in  tb.devices:
+    temp = tb.devices[router]
+    temp.connect()
+    bgpParse = temp.parse('show bgp summary')
+    output = nested_lookup('vrf', bgpParse)
+    output2 = {router : output[0]}
+    outputDict.update(output2)
+
+for spam, eggs in outputDict.items():
+    print (str(spam) + str(eggs['default']['neighbor'].keys()))
+
+
+#pprint(outputDict)
+#pprint(bgpParse)
