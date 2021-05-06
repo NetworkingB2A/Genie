0f76234082aa84477db708f2668d4e50da3a7cba
Adam Angell <adam@networkingb2a.com>
2021-05-05 23:52:57 -0500

    Getting there. TB file almost there

--- utils/get_from_netbox.py
+++ utils/get_from_netbox.py
@@ -7,22 +7,11 @@ netbox_token = os.getenv("NETBOX_TOKEN")
 netbox_url = os.getenv("NETBOX_URL")
 
 netbox = pynetbox.api(netbox_url, token=netbox_token)
-devices = netbox.dcim.devices.all()
+devices = netbox.dcim.devices.filter(role=["lab-router", "lab-switch"])
+#devices = netbox.dcim.devices.all()
 for device in devices:
-    print(device)
+    pprint(dict(device))
+    print(device.custom_fields["ansible_password"])
+    testbed= {"name": device.display_name, "ip address": device.primary_ip["address"]}
+    print (testbed)
 
-
-
-#
-#url = "http://angell-netbox:8000/api/dcim/devices/?tag=labprod"
-#
-#payload={}
-#headers = {
-#  'Content-Type': 'application/json',
-#  'Authorization': 'Token b55fabbaa93e38f8fc762a32744ff0198662fc3f'
-#}
-
-#response = requests.request("GET", url, headers=headers, data=payload)
-#test = response.json()['results']
-#for number, device in enumerate(test):
-#    pprint(test[number])
