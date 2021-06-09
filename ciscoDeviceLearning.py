4a9932e31154f0bd8e058ccd13a8edd401778c7c
Adam Angell <adam@networkingb2a.com>
2021-06-08 22:08:56 -0500

    Changed Variables in class

--- ciscoDeviceLearning.py
+++ ciscoDeviceLearning.py
@@ -4,22 +4,13 @@ from utils.get_from_netbox import netboxget
 from utils.learnIOSInfo import LearnIOSInfo
 from genie.testbed import load
 
-
-# CDP = LearnCDPNeighbor(netboxget())
-# git test
-
-
 tb = load(netboxget())
 
 print("starting learns")
 for device in tb.devices:
     tempConnect = tb.devices[device]
-    tempConnect.connect(log_stdout=False)
+    tempConnect.connect(log_stdout=True)
     networkObject = LearnIOSInfo(tempConnect, device)
     networkObject.LearningRouting()
     networkObject.LearningCDPNeighbors()
-
-
-# print('learning CDP Neighbors')
-# CDP.LearningCDPNeighbors()
 print("learn complete")
--- utils/learnIOSInfo.py
+++ utils/learnIOSInfo.py
@@ -4,28 +4,26 @@ from pprint import pprint
 from nested_lookup import nested_lookup
 
 
+
+
 class LearnIOSInfo:
+    
     def __init__(self, networkDevice, deviceName):
         self.networkDevice = networkDevice
         self.deviceName = deviceName
 
     def LearningRouting(self):
-        tb = self.networkDevice
-        hostname = self.deviceName
-        ipRoutes = tb.parse("show ip route ")
+        ipRoutes = self.networkDevice.parse("show ip route ")
         routes = nested_lookup("routes", ipRoutes)[0]
-        with open(f"Outputs/learned_routes_{hostname}.txt", "w") as tempFile:
+        with open(f"Outputs/learned_routes_{self.deviceName}.txt", "w") as tempFile:
             for (routeTemp, routeFacts) in routes.items():
                 tempFile.write(
                     str(f'{routeTemp} is a { routeFacts["source_protocol"]} route\n')
                 )
 
     def LearningCDPNeighbors(self):
-        tb = self.networkDevice
-        hostname = self.deviceName
-
-        cdpNeighbors = tb.parse("show cdp neighbors detail")
-        with open(f"Outputs/learned_cdp_neighbors_{hostname}.txt", "w") as tempFile:
+        cdpNeighbors = self.networkDevice.parse("show cdp neighbors detail")
+        with open(f"Outputs/learned_cdp_neighbors_{self.deviceName}.txt", "w") as tempFile:
             for key, value in cdpNeighbors["index"].items():
                 neighborName = nested_lookup("device_id", value)[0]
                 try:
@@ -40,6 +38,6 @@ class LearnIOSInfo:
                 neighborInterface = nested_lookup("port_id", value)[0]
                 tempFile.write(
                     str(
-                        f"{hostname} has port {localInterface} connecteed to {neighborName}'s on port {neighborInterface}\n"
+                        f"{self.deviceName} has port {localInterface} connecteed to {neighborName}'s on port {neighborInterface}\n"
                     )
                 )
